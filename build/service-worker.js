88b837bb7b1df86a5e264b196c420b9fd348a61d
Ilmari <eoksanen@gmail.com>
2020-04-12 19:52:35 +0300

    front2

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c0c64b015a0f5993e73e4b38762f86e.js"
+  "/precache-manifest.153f11d4dc5ea9dcb87ef60f7ca9acc0.js"
 );
 
 self.addEventListener('message', (event) => {
